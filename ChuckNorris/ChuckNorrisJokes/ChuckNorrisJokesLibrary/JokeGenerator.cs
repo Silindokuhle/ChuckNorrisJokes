7b0e011aee8c6f93957e333a28d5d44e8462809f
Silindokuhle <3366171@myuwc.ac.za>
2019-10-02 15:11:45 +0200

    Initial Chuck Norris Commit

---
+++ ChuckNorris/ChuckNorrisJokes/ChuckNorrisJokesLibrary/JokeGenerator.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisJokesLibrary
+{
+    public class JokeGenerator
+    {
+        // (1)public string GetRandomJokes()
+        public async Task<string[]> GetCategories()
+        {
+            //(1) var client = new HttpClient();
+           
+
+            // string randomJoke = await client.GetStringAsync("https://api.chucknorris.io/jokes/random");
+
+            //client.GetStringAsync(https://api.chucknorris.io/jokes/random);
+
+            //(1)return "Will return joke later.";
+            //return randomJoke;
+
+            string categoryString = await client.GetStringAsync("https://api.chucknorris.io/jokes/category");
+            var categories = JsonConvert.DeserializeObject<string[]>(categoryString);
+
+            return categories;
+        }
+
+        public Task<string[]> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+  
